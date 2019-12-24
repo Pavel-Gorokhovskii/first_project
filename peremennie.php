0f1cd38aba01fb5651ae1bacd84a174108b018c3
Pavel-Gorokhovskii <Pavel-Gorokhovskii@yandex.ru>
2019-12-24 20:58:44 +0300

    Update

--- peremennie.php
+++ peremennie.php
@@ -9,8 +9,23 @@
 <body>
     <?php
     $a=5;
-    $a<<1;
-    echo $a;
+    $c=$a<<1;
+    echo $c;
+    echo"<br>";
+    $bool = TRUE; // логический
+    $str = "foo"; // строковый
+    $int = 12; // целочисленный
+    echo gettype ($bool); // выводит "boolean"
+    echo gettype ($str); // выводит "string"
+    // Если это целое, увеличить на четыре
+    if (is_int($int)) {
+        $int += 4;
+    }
+    // Если $bool - это строка, вывести ее
+    // (ничего не выводит)
+    if (is_string($bool)) {
+        echo "Строка: $bool";
+    }
     ?>
     
 </body>
