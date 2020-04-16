3582ade303d425ec1bab893619ff13b24459f0ed
Pavel <59092937+Pavel-Gorokhovskii@users.noreply.github.com>
2020-04-16 10:23:03 +0300

    rectangle, triangle, circle

--- square/calc.php
+++ square/calc.php
@@ -1,5 +1,18 @@
 <?php
 include("autoload.php");
 $result = new Square(5);
-echo $result->perimetr() . "\n";
-echo $result->area();
+echo "квадрат <br>";
+echo "периметр квадрата " . $result->perimetr() . "<br>";
+echo "площадь квадрата " . $result->area() . "<br>";
+$rectangle = new Rectangle(3, 4);
+echo "прямоугольник <br>";
+echo "периметр прямоугольника " . $rectangle->perimetr() . "<br>";
+echo "площадь прямоугольника " . $rectangle->area() . "<br>";
+$triangle = new Triangle(3, 4, 5);
+echo "треугольник <br>";
+echo "периметр треугольника " . $triangle->perimetr() . "<br>";
+echo "площадь треугольника " . $triangle->area() . "<br>";
+$circle = new Circle(5);
+echo "окружность <br>";
+echo "периметр окружности " . $circle->perimetr() . "<br>";
+echo "площадь окружности " . $circle->area() . "<br>";
