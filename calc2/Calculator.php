510506fbf80df27eb74c717994aa6a2bcb666e4f
Pavel-Gorokhovskii <Pavel-Gorokhovskii@yandex.ru>
2020-07-24 20:05:36 +0300

    Update Calculator.php

--- calc2/Calculator.php
+++ calc2/Calculator.php
@@ -8,19 +8,19 @@ class Calculator
         $this->a = $a;
         $this->b = $b;
     }
-    public function sum()
+    public function sum(): float
     {
         return $this->a + $this->b;
     }
-    public function mul()
+    public function mul(): float
     {
         return $this->a * $this->b;
     }
-    public function dif()
+    public function dif(): float
     {
         return $this->a - $this->b;
     }
-    public function div()
+    public function div(): float
     {
         return $this->a / $this->b;
     }
